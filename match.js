d463a8b0f0c54bf42d3a6aa56c2509ba4cafa59f
bohehxx <2674680690@qq.com>
2020-10-28 09:23:58 +0800

    Update match.js

--- match.js
+++ match.js
@@ -1,8 +1,7 @@
 export function pick(str) {
-  var reg = /@#underline:([^\s]*?)\#@/g
+  var reg = /@#under:([^\s]*?)\#@/g
   var ret = []
   var arr
-  // eslint-disable-next-line
     while ((arr = reg.exec(str))) ret.push(arr[1]);
   return ret
 }
